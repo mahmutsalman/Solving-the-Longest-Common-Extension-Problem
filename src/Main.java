84fdd8ce524a6c649a1ba86f346f91c504babea1
mahmut <leipzig9998@hotmail.com>
2022-12-03 14:44:34 +0300

    inital commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //take two text files as input and process them process
+
+    }
+    public void process(String file1, String file2){
+        //process the two files and return the result
+
+    }
+    // a method to make the longest common extension
+    public String findLCS(String file1, String file2){
+        //find the longest common extension
+
+        return "LCS";
+    }
+
+}
